3301660db7cd7f86e8ec93bfa26309286c2681a2
Ankush <168534792+Ankush135@users.noreply.github.com>
2025-12-07 13:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
+
+
+  if (!age || !name) {
+    alert("Please enter valid details");
+    return;
+  }
+
+  const votingPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  votingPromise
+    .then((message) => alert(message))
+    .catch((err) => alert(err));
+});
